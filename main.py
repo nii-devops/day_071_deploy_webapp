3f9c0fe85524c9195d572944ec9d4492dce17de9
Benjamin <niiakoadjei.devops@gmail.com>
2024-06-05 22:54:53 +0000

    New env variables set

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 
-app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
+#app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
 
 ckeditor = CKEditor(app)
 Bootstrap5(app)
@@ -68,7 +68,7 @@ class Base(DeclarativeBase):
 
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
